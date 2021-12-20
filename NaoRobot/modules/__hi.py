8d951c8fe7517fdbc42df2032af2a21befd5739e
Harsh Pandit <93505687+HarshPanditXD@users.noreply.github.com>
2021-12-20 11:25:10 +0530

    Create __hi.py

---
+++ NaoRobot/modules/__hi.py
@@ -0,0 +1,20 @@
+import re
+import os
+
+from telethon import events, Button
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from NaoRobot.events import register as MEMEK
+from NaoRobot import telethn as tbot
+
+
+@MEMEK(pattern=("/Hello"))
+async def awake(event):
+  tai = event.sender.first_name
+  NAO = f"**H╔┓┏╦━╦┓╔┓╔━━╗
+║┗┛║┗╣┃║┃║X X║
+║┏┓║┏╣┗╣┗╣╰╯║
+╚┛┗╩━╩━╩━╩━━╝**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Hypermen_rbot?start=help"), Button.url("Sᴜᴘᴘᴏʀᴛ", "https://t.me/UNIQUE_SOCIETY")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=NAO,  buttons=BUTTON)
+0
